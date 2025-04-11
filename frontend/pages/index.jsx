0e3915b6d9bf8f25480645a1ab7e78c0b19c6aa1
Bill-pngh <grabillrose078@gmail.com>
2025-04-11 19:34:25 +0100

    Create index.jsx

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,10 @@
+import MainLayout from '../components/layout/MainLayout';
+import GameContainer from '../components/game/GameContainer';
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <GameContainer />
+    </MainLayout>
+  );
+}
