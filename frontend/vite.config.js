14752ba8403f5616f567ced2deab919bcce7e934
Bill-pngh <grabillrose078@gmail.com>
2025-04-11 23:01:40 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,12 +14,22 @@ export default defineConfig({
   define: {
     global: 'window' // Required for Telegram WebApp
   },
+  server: {
+    port: 3000,
+    strictPort: true
+  },
   build: {
     target: 'esnext',
     minify: 'terser',
     sourcemap: true,
+    assetsInlineLimit: 4096, // 4kb - from second config
     rollupOptions: {
       output: {
+        // From second config
+        entryFileNames: `[name].[hash].js`,
+        chunkFileNames: `[name].[hash].js`,
+        assetFileNames: `[name].[hash].[ext]`,
+        // From first config
         manualChunks: {
           solana: ['@solana/web3.js'],
           charts: ['chart.js', 'react-chartjs-2']
